4f6a04954fa635802b0ea014b1d901214b83becf
2Strong4them <91446777+2Strong4them@users.noreply.github.com>
2024-08-08 10:24:32 +0200

    re-usable buton added

--- steps-project/src/App.js
+++ steps-project/src/App.js
@@ -5,9 +5,25 @@ const messages = [
   "Apply for jobs 💼",
   "Invest your new income 🤑",
 ];
-//? The useState function returns an array with two elements: the current state and a function to update that current state. The array indexes are 0 and 1 respectively. When destructured with [],the first element is the current state and the second element is the function to update that current state. Hooks can only be called at the top level of a component/function.
 
 export default function App() {
+  return (
+    <div>
+      <Steps />
+      <StepMessage step={1}>
+        <p>Pass in content</p>
+        <p>👩‍🦰</p>
+      </StepMessage>
+      <StepMessage step={2}>
+        <p>Read children prop</p>
+        <p>😍</p>
+      </StepMessage>
+    </div>
+  );
+}
+//? The useState function returns an array with two elements: the current state and a function to update that current state. The array indexes are 0 and 1 respectively. When destructured with [],the first element is the current state and the second element is the function to update that current state. Hooks can only be called at the top level of a component/function.
+
+function Steps() {
   const [step, setStep] = useState(1);
   const [isOpen, setIsOpen] = useState(true); //isOpen = true
 
@@ -37,26 +53,51 @@ export default function App() {
             <div className={step >= 3 ? "active" : ""}>3</div>
           </div>
 
-          <p className="message">
-            Step{step}:{messages[step - 1]}
-          </p>
+          <StepMessage step={step}>
+            {messages[step - 1]}
+            <div className="buttons">
+              <Button
+                bgColor="#e7e7e7"
+                textColor="black"
+                onClick={() => alert(`learn hot to ${messages[step - 1]}`)}
+              >
+                Learn how
+              </Button>
+            </div>
+          </StepMessage>
 
           <div className="buttons">
-            <button
-              style={{ backgroundColor: "#7950f2", color: "#fff" }}
-              onClick={handlePrevious}
-            >
-              Previous
-            </button>
-            <button
-              style={{ backgroundColor: "#7950f2", color: "#fff" }}
-              onClick={handleNext}
-            >
-              Next
-            </button>
+            <Button bgColor="#7950f2" textColor="#fff" onClick={handlePrevious}>
+              <span>👈</span>Previous
+            </Button>
+            <Button bgColor="#7950f2" textColor="#fff" onClick={handleNext}>
+              Next<span>👉</span>
+            </Button>
           </div>
         </div>
       )}
     </>
+  );
+}
+
+/*THE RE-USABLE STEP MESSAGE: */
+function StepMessage({ step, children }) {
+  return (
+    <div className="message">
+      <h3>Step{step}</h3>
+      {children}
+    </div>
+  );
+}
+
+/*THE RE-USABLE BUTTON: here we want to instead accepts props for bg color, onclick, text color and emoji instead of the values to make them re-usable*/
+function Button({ textColor, bgColor, onClick, children }) {
+  return (
+    <button
+      style={{ backgroundColor: bgColor, color: textColor }}
+      onClick={onClick}
+    >
+      {children}
+    </button>
   );
 }
